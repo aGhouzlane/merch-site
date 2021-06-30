735a3c881fdaa19641117d350f6e50c0a4294d62
aGhouzlane <ahmedghouzlane@gmail.com>
2021-06-30 17:27:44 -0400

    Add style to header
    Co-authored-by: Seth Medeiros <MedeirosSeth@gmail.com>

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,8 +1,18 @@
 import React from "react";
 
 function Header() {
+  const footerStyle = {
+    backgroundColor: 'rgb(168, 255, 247)',
+    marginTop: '0px',
+    padding: '10px',
+    display: 'flex',
+    alignItems: 'center'
+  }
   return (
-    <h1>T-Shirt Shop</h1>
+    <div style={footerStyle}>
+      <h1>T-Shirt Shop</h1>
+    </div>
+
   );
 }
 
